cae2838869f2571fb6677357040c54532eff7ea1
SodaHome <sobhan.dar@solid.lth.se>
2026-01-15 11:00:10 +0100

    added webscraping

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import sys
-print(sys.executable)
+from bs4 import BeautifulSoup
+import requests
+
+# Get html file and parse it
+page = requests.get("https://www.ceq.lth.se/rapporter/ceq/2024_HT/LP1/KBKN05_2024_HT_LP1_slutrapport.html")
+soup = BeautifulSoup(page.text, "lxml")
+
+# Find relevant data
+table = soup.find_all("table")[6]
+rows = table.find_all("tr")
+cells = rows[1].find_all("td")
+
+print(rows)
